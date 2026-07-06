312dfc47432323be787478be36f1e40d4355e5bc
Mihaela <mihaela.dimitrova_@abv.bg>
2026-07-06 20:29:09 +0300

    Initial commit

---
+++ src/test/java/com/mihaela/orderplatform/transaction_service/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihaela.orderplatform.transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
